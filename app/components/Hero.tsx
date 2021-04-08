6e25d3c2fa5a3d25dabbe8c815741afda3cdb672
Alexandru Marcus <alexmarcus20141@gmail.com>
2021-04-08 17:10:01 +0300

    finish wip

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,96 @@
-import { Header } from "../shared/Header";
-import styled from "../styles/components/hero.module.scss";
+import { HeaderComponent } from "../shared/Header";
+import { SearchBar } from "../components/SearchBar";
+import styled from "styled-components";
 
 export const Hero = () => {
+  const title = "Krusn 2 U";
+  const description = "Our services. Your Schedule";
   return (
-    <section className={styled.heroComponent}>
+    <HeroComponent>
       <div className="container">
-        <Header />
+        <div className="hero-header">
+          <HeaderComponent />
+          <SearchBar></SearchBar>
+        </div>
+        <div className="hero-content">
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+          <div className="row">
+            <div className="col text-center d-flex d-lg-none justify-content-center">
+              <ButtonHero>Browse Inventory</ButtonHero>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </HeroComponent>
   );
 };
+
+const HeroComponent = styled.div`
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.81) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    url("images/bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+
+  .hero-header {
+    margin-bottom: 12rem;
+  }
+
+  .hero-content {
+  }
+`;
+
+const ButtonHero = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0.8rem 10rem;
+  background: rgba(255, 255, 255, 0.15);
+  border: 1px solid rgba(255, 255, 255, 0.6);
+  box-sizing: border-box;
+  border-radius: 3px;
+  position: relative;
+  font-size: 1.8rem;
+  line-height: 2.8rem;
+  color: white;
+  font-style: normal;
+  font-weight: 600;
+`;
+
+const Title = styled.h1`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 5.6rem;
+  line-height: 5.6rem;
+  letter-spacing: 0.02em;
+  font-feature-settings: "pnum" on, "lnum" on;
+  color: #ffffff;
+
+  @media (max-width: 991px) {
+    font-size: 3.6rem;
+    line-height: 4.8rem;
+  }
+`;
+
+const Description = styled.p`
+  color: white;
+  font-size: 3.2rem;
+  line-height: 4.4rem;
+  letter-spacing: 0.02em;
+  font-feature-settings: "pnum" on, "lnum" on;
+
+  @media (max-width: 991px) {
+    margin-bottom: 17rem;
+    font-size: 2rem;
+    line-height: 3.2rem;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-// import style from "styled-components";
 import { Hero } from "../app/components/Hero";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -9,31 +8,37 @@ export default function Home() {
       <Head>
         <title>Kruse </title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Hero />
       <style jsx global>{`
         html {
           font-size: 10px;
         }
+
         body {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          font-weight: 400;
-          line-height: 1.8;
-          color: #333;
-          font-family: sans-serif;
-        }
-        h1 {
-          font-weight: 700;
-        }
-        p {
-          margin-bottom: 10px;
-        }
-        .container {
-          max-width: 1320px;
+          font-size: 10px;
         }
       `}</style>
+      <script
+        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossOrigin="anonymous"
+      ></script>
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossOrigin="anonymous"
+      ></script>
+      <script
+        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossOrigin="anonymous"
+      ></script>
     </div>
   );
 }
